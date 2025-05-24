eace9208dc00b8a3b00b1ffdc5c6d54f6ba77291
Christian Genesis A. Diomampo <chrisgen19@gmail.com>
2025-05-24 14:50:10 +0800

    add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,12 +13,14 @@ export default function App({ Component, pageProps }: AppProps) {
         
         {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         
         {/* Theme Color */}
         <meta name="theme-color" content="#667eea" />
+        <meta name="msapplication-TileColor" content="#667eea" />
         
         {/* Verification Tags (add when you have accounts) */}
         {/* <meta name="google-site-verification" content="your-verification-code" /> */}
